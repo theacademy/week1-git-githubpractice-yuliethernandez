4600dbad547233be919147909952be493e485159
Yuliet Hernandez <yuliethdez2020@gmail.com>
2023-05-09 16:20:01 +0100

    Hello world class Yuliet

---
+++ NetBeans Week 1/HelloWorld2/src/main/java/com/wileyedge/yuliet/helloworld2/HelloWorld2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.wileyedge.yuliet.helloworld2;
+
+/**
+ *
+ * @author nelso
+ */
+public class HelloWorld2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello class...Yuliet Hernandez");
+    }
+}
