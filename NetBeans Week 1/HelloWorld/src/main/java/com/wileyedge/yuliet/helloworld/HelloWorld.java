0727b9575c1e5d5f2ce96a39f35372367d3e9f8d
Yuliet Hernandez <yuliethdez2020@gmail.com>
2023-05-09 14:24:04 +0100

    Hello world week 1 class 2

---
+++ NetBeans Week 1/HelloWorld/src/main/java/com/wileyedge/yuliet/helloworld/HelloWorld.java
@@ -0,0 +1,15 @@
+/**
+ *@author Yuliet Hernandez Exposito
+ *email: yuliethdez2020@gmail.com
+ *date: 09/05/2023
+ *purpose: My first Hello World program in an IDE
+ */
+package com.wileyedge.yuliet.helloworld;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello class");
+    }
+    
+}
